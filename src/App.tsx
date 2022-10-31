986ee91e48ba49bd485d084986e6b1b451823713
DavePersonal <david.surina22@gmail.com>
2022-10-31 23:17:02 +0100

    added inputs into form and adjusted styles

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,15 @@ function App(): JSX.Element {
   return (
     <div className="app">
       <header className="header">
-        <h1 className="main_heading">Idea Board</h1>
+        <h1>Idea Board</h1>
       </header>
       <body className="body">
-        <div className="input_wrapper">
-          <input className="input" value={userInput} placeholder="Idea title" onChange={e => setUserInput(e.target.value)} maxLength={140}/>
-          <textarea className="textarea" maxLength={140} placeholder="Idea title description" draggable={false}/>
-          <button className="input_btn">Add idea</button>
-        </div>
+        <form className="input_wrapper">
+          <input className="input" value={userInput} placeholder="Idea title" onChange={e => setUserInput(e.target.value)} minLength={5} maxLength={30}/>
+          <textarea className="textarea" minLength={10} maxLength={140} rows={3} placeholder="Idea title description" />
+          <button className="input_btn" type='submit'>Add idea</button>
+        </form>
+        <div className='divider' />
         <div className="boards_wrapper">
 
         </div>
