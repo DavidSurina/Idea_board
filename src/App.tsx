d240de385d0f1ba0025b2566e23d1071bb963201
DavePersonal <david.surina22@gmail.com>
2022-11-03 21:23:06 +0100

    added logic to save user Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,92 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useEffect, useReducer, useRef } from "react";
+import "./App.css";
+import IdeaCard from "./Components/IdeaCard/IdeaCard";
+
+export type ideaState = {
+  title: string;
+  textArea: string;
+  changed: Date | undefined;
+};
 
 function App(): JSX.Element {
-  const [userInput, setUserInput ] = useState('');
+  const titleInputRef = useRef<HTMLInputElement>();
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+
+  const [items, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case "add":
+        return [
+          ...state,
+          {
+            id: state.length,
+            title: action.title,
+            textArea: action.textArea,
+            changed: action.changed,
+          },
+        ];
+      case "delete":
+        return state.filter((_, index) => index !== action.index);
+      default:
+        return state;
+    }
+  }, []);
 
   useEffect(() => {
-    console.log(userInput);
-  }, [userInput])
-  
+    console.log(items);
+  }, [items]);
+
+  function handleSubmit(e): void {
+    e.preventDefault();
+    if (
+      titleInputRef.current.value !== "" &&
+      textAreaRef.current.value !== ""
+    ) {
+      dispatch({
+        type: "add",
+        title: titleInputRef?.current?.value,
+        textArea: textAreaRef?.current?.value,
+        changed: new Date().toLocaleString(),
+      });
+      titleInputRef.current.value = "";
+      textAreaRef.current.value = "";
+    }
+  }
+
+  const boardsRender =
+    items.length > 0 ? (
+      items.map((idea) => <IdeaCard ideaObj={idea} setter={dispatch} />)
+    ) : (
+      <div>No ideas yet</div>
+    );
+
   return (
     <div className="app">
       <header className="header">
         <h1>Idea Board</h1>
       </header>
       <body className="body">
-        <form className="input_wrapper">
-          <input className="input" value={userInput} placeholder="Idea title" onChange={e => setUserInput(e.target.value)} minLength={5} maxLength={30}/>
-          <textarea className="textarea" minLength={10} maxLength={140} rows={3} placeholder="Idea title description" />
-          <button className="input_btn" type='submit'>Add idea</button>
+        <form className="input_wrapper" onSubmit={handleSubmit}>
+          <input
+            ref={titleInputRef}
+            className="input"
+            placeholder="Idea title"
+            minLength={5}
+            maxLength={30}
+          />
+          <textarea
+            ref={textAreaRef}
+            className="textarea"
+            minLength={10}
+            maxLength={140}
+            rows={3}
+            placeholder="Idea title description"
+          />
+          <button className="input_btn" type="submit">
+            Add idea
+          </button>
         </form>
-        <div className='divider' />
-        <div className="boards_wrapper">
-
-        </div>
+        <div className="divider" />
+        <div className="board_wrapper">{boardsRender}</div>
       </body>
     </div>
   );
